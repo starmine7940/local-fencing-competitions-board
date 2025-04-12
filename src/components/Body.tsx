dcb3d3aae75c8a3ca4357a09f9553ffd418d227e
starmine7940 <starmine7940@gmail.com>
2025-04-13 04:22:49 +0900

    Chakra UI の導入

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,7 @@
 // import { createUseStyles } from "react-jss"
 import { Firestore } from "firebase/firestore"
 import { useGetCompetitions } from "../hooks/useGetCompetions"
+import { Table } from "@chakra-ui/react"
 
 // const useStyles = createUseStyles({})
 
@@ -12,7 +13,24 @@ export const Body = ({db}: BodyArgs) => {
   // const classes = useStyles()
 
   const {competitions} = useGetCompetitions({db})
-  console.log(competitions)
 
-  return <div>body</div>
+  return (
+    <Table.Root>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeader>大会名</Table.ColumnHeader>
+          <Table.ColumnHeader>登録日</Table.ColumnHeader>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {competitions.map((competition) => (
+          <Table.Row key={competition.name}>
+            <Table.Cell>{competition.name}</Table.Cell>
+            <Table.Cell>{competition.createdAt.toLocaleString()}</Table.Cell>
+
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
