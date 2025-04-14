5d12b50d0d3ab997e3a6471d6cef76ce09da5da4
starmine7940 <starmine7940@gmail.com>
2025-04-14 22:59:54 +0900

    テーブルを別コンポーネントに切り出した

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,7 @@
 // import { createUseStyles } from "react-jss"
 import { Firestore } from "firebase/firestore"
 import { useGetCompetitions } from "../hooks/useGetCompetions"
-import { Table } from "@chakra-ui/react"
+import { List } from "./List"
 
 // const useStyles = createUseStyles({})
 
@@ -16,35 +16,6 @@ export const Body = ({db}: BodyArgs) => {
   console.log(competitions)
 
   return (
-    <Table.Root>
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader>大会名</Table.ColumnHeader>
-          <Table.ColumnHeader>年齢区分</Table.ColumnHeader>
-          <Table.ColumnHeader>種目</Table.ColumnHeader>
-          <Table.ColumnHeader>開催日</Table.ColumnHeader>
-          <Table.ColumnHeader>申込締切日</Table.ColumnHeader>
-          <Table.ColumnHeader>URL</Table.ColumnHeader>
-          <Table.ColumnHeader>備考</Table.ColumnHeader>
-          <Table.ColumnHeader>登録日</Table.ColumnHeader>
-          <Table.ColumnHeader>削除</Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {competitions.map((competition) => (
-          <Table.Row key={competition.id}>
-            <Table.Cell>{competition.name}</Table.Cell>
-            <Table.Cell>{competition.ageCategory}</Table.Cell>
-            <Table.Cell>{competition.eventCategory}</Table.Cell>
-            <Table.Cell>{competition.date.toDateString()}</Table.Cell>
-            <Table.Cell>{competition.subscriptionDeadlineDate.toDateString()}</Table.Cell>
-            <Table.Cell>{competition.url}</Table.Cell>
-            <Table.Cell>{competition.notes}</Table.Cell>
-            <Table.Cell>{competition.registrationDate.toDateString()}</Table.Cell>
-            <Table.Cell>🗑️</Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+    <List competitions={competitions}/>
   )
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,46 @@
+// import { createUseStyles } from "react-jss"
+import { Table } from "@chakra-ui/react"
+import { Competition } from "../util/types"
+
+// const useStyles = createUseStyles({})
+
+type ListArgs = {
+  competitions: Competition[],
+}
+
+export const List = ({competitions}: ListArgs) => {
+  // const classes = useStyles()
+
+  return (
+    <Table.Root>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeader>大会名</Table.ColumnHeader>
+          <Table.ColumnHeader>年齢区分</Table.ColumnHeader>
+          <Table.ColumnHeader>種目</Table.ColumnHeader>
+          <Table.ColumnHeader>開催日</Table.ColumnHeader>
+          <Table.ColumnHeader>申込締切日</Table.ColumnHeader>
+          <Table.ColumnHeader>URL</Table.ColumnHeader>
+          <Table.ColumnHeader>備考</Table.ColumnHeader>
+          <Table.ColumnHeader>登録日</Table.ColumnHeader>
+          <Table.ColumnHeader>削除</Table.ColumnHeader>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {competitions.map((competition) => (
+          <Table.Row key={competition.id}>
+            <Table.Cell>{competition.name}</Table.Cell>
+            <Table.Cell>{competition.ageCategory}</Table.Cell>
+            <Table.Cell>{competition.eventCategory}</Table.Cell>
+            <Table.Cell>{competition.date.toDateString()}</Table.Cell>
+            <Table.Cell>{competition.subscriptionDeadlineDate.toDateString()}</Table.Cell>
+            <Table.Cell>{competition.url}</Table.Cell>
+            <Table.Cell>{competition.notes}</Table.Cell>
+            <Table.Cell>{competition.registrationDate.toDateString()}</Table.Cell>
+            <Table.Cell>🗑️</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  )
+}
