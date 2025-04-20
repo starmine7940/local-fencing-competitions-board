9757ef49e7162e7055407c0ca3115bb4d3187c37
starmine7940 <starmine7940@gmail.com>
2025-04-20 22:03:54 +0900

    Table の細かい調整

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -12,46 +12,48 @@ export const List = ({ competitions }: ListArgs) => {
   // const classes = useStyles()
 
   return (
-    <Table.Root>
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader>大会名</Table.ColumnHeader>
-          <Table.ColumnHeader>会場</Table.ColumnHeader>
-          <Table.ColumnHeader>種目</Table.ColumnHeader>
-          <Table.ColumnHeader>性別</Table.ColumnHeader>
-          <Table.ColumnHeader>年齢区分</Table.ColumnHeader>
-          <Table.ColumnHeader>大会開始日</Table.ColumnHeader>
-          <Table.ColumnHeader>大会終了日</Table.ColumnHeader>
-          <Table.ColumnHeader>申込締切日</Table.ColumnHeader>
-          <Table.ColumnHeader>URL</Table.ColumnHeader>
-          <Table.ColumnHeader>備考</Table.ColumnHeader>
-          <Table.ColumnHeader>登録日</Table.ColumnHeader>
-          {/* <Table.ColumnHeader>削除</Table.ColumnHeader> */}{' '}
-          {/* TODO: 削除機能の実装 */}
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {competitions.map((competition) => (
-          <Table.Row key={competition.id}>
-            <Table.Cell>{competition.name}</Table.Cell>
-            <Table.Cell>{competition.site}</Table.Cell>
-            <Table.Cell>{competition.eventCategory}</Table.Cell>
-            <Table.Cell>{competition.genderCategory}</Table.Cell>
-            <Table.Cell>{competition.ageCategory}</Table.Cell>
-            <Table.Cell>{competition.startDate?.toDateString()}</Table.Cell>
-            <Table.Cell>{competition.finishDate?.toDateString()}</Table.Cell>
-            <Table.Cell>
-              {competition.subscriptionDeadlineDate?.toDateString()}
-            </Table.Cell>
-            <Table.Cell>{competition.url}</Table.Cell>
-            <Table.Cell>{competition.notes}</Table.Cell>
-            <Table.Cell>
-              {competition.registrationDate.toDateString()}
-            </Table.Cell>
-            {/* <Table.Cell>🗑️</Table.Cell> */} {/* TODO: 削除機能の実装 */}
+    <Table.ScrollArea borderWidth="1px"> {/* TODO: 最大幅の指定 */}
+      <Table.Root showColumnBorder>
+        <Table.Header>
+          <Table.Row bg="bg.subtle">
+            <Table.ColumnHeader>大会名</Table.ColumnHeader>
+            <Table.ColumnHeader>会場</Table.ColumnHeader>
+            <Table.ColumnHeader>種目</Table.ColumnHeader>
+            <Table.ColumnHeader>性別</Table.ColumnHeader>
+            <Table.ColumnHeader>年齢区分</Table.ColumnHeader>
+            <Table.ColumnHeader>大会開始日</Table.ColumnHeader>
+            <Table.ColumnHeader>大会終了日</Table.ColumnHeader>
+            <Table.ColumnHeader>申込締切日</Table.ColumnHeader>
+            <Table.ColumnHeader>URL</Table.ColumnHeader>
+            <Table.ColumnHeader>備考</Table.ColumnHeader>
+            <Table.ColumnHeader>登録日</Table.ColumnHeader>
+            {/* <Table.ColumnHeader>削除</Table.ColumnHeader> */}{' '}
+            {/* TODO: 削除機能の実装 */}
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Header>
+        <Table.Body>
+          {competitions.map((competition) => (
+            <Table.Row key={competition.id}>
+              <Table.Cell>{competition.name}</Table.Cell>
+              <Table.Cell>{competition.site}</Table.Cell>
+              <Table.Cell>{competition.eventCategory}</Table.Cell>
+              <Table.Cell>{competition.genderCategory}</Table.Cell>
+              <Table.Cell>{competition.ageCategory}</Table.Cell>
+              <Table.Cell>{competition.startDate?.toDateString()}</Table.Cell>
+              <Table.Cell>{competition.finishDate?.toDateString()}</Table.Cell>
+              <Table.Cell>
+                {competition.subscriptionDeadlineDate?.toDateString()}
+              </Table.Cell>
+              <Table.Cell>{competition.url}</Table.Cell>
+              <Table.Cell>{competition.notes}</Table.Cell>
+              <Table.Cell>
+                {competition.registrationDate.toDateString()}
+              </Table.Cell>
+              {/* <Table.Cell>🗑️</Table.Cell> */} {/* TODO: 削除機能の実装 */}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Table.ScrollArea>
   )
 }
