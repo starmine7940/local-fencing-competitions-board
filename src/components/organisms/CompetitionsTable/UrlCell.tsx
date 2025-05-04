10155c57a505eb5db0bcdc41e7e901e0038876ca
starmine7940 <starmine7940@gmail.com>
2025-05-04 22:17:38 +0900

    URL の外部アイコンを削除

--- src/components/organisms/CompetitionsTable/UrlCell.tsx
+++ src/components/organisms/CompetitionsTable/UrlCell.tsx
@@ -1,5 +1,4 @@
 import { Box, Link } from '@chakra-ui/react'
-import { LuExternalLink } from 'react-icons/lu'
 
 type UrlCellArgs = {
   url: string
@@ -12,13 +11,13 @@ export const UrlCell = ({ url }: UrlCellArgs) => {
   return (
     <Box wordBreak="break-word" whiteSpace="normal">
       <Link href={url} target="_blank">
-        {preview} <LuExternalLink />
+        {preview}
       </Link>
       {isLong && (
         <details>
           <summary>全文はこちら</summary>
           <Link href={url} target="_blank">
-            {url} <LuExternalLink />
+            {url}
           </Link>
         </details>
       )}
